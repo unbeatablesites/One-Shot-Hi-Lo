7486584ef99c6babe87861df38c966865644e029
Frank Uzoka <Frankuzoka@hotmail.com>
2020-03-05 19:22:05 -0500

    Adding logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,21 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-
+        doLogic();
     }
 
     public static void doLogic(){
 
         System.out.println("I'm thinking of a number between 1-100.  Try to guess it.");
 
+        Scanner scan = new Scanner(System.in);
+
+        int userInput = scan.nextInt();
+
         Random rand = new Random();
-
-
-
-
 
     }
 
