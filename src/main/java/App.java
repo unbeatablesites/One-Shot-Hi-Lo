b1be2e92374241658b438324e6aba587e780cebf
Frank Uzoka <Frankuzoka@hotmail.com>
2020-03-05 19:49:02 -0500

    Adding logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,13 +9,27 @@ public class App {
 
     public static void doLogic(){
 
-        System.out.println("I'm thinking of a number between 1-100.  Try to guess it.");
-
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("I'm thinking of a number between 1-100.  Try to guess it.");
 
         int userInput = scan.nextInt();
 
         Random rand = new Random();
+
+        int randomNumber = 1 +rand.nextInt(100);
+
+        if(randomNumber == userInput){
+
+            System.out.println("You guessed it!  What are the odds?!?");
+
+        }else if (randomNumber > userInput){
+
+            System.out.println("Sorry, you are too low.  I was thinking of " + randomNumber);
+        }else{
+            System.out.println("Sorry, you are too High.  I was thinking of " + randomNumber);
+        }
+
 
     }
 
